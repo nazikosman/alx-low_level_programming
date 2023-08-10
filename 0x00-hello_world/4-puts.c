99cdbc3e0c6814dd3d517fef4bdc289125db176e
nazik <naziktajalsir1999@gmail.com>
2023-08-10 13:50:17 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints py put function
+ * Description: A C program that prints with put function
  *
- * Return: Alawys 0 (Success)
+ * Return: Alawys 0 (success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+
+}
+
