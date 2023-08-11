c7994b3a3ef34dbe8c0d26c24789b2543a112854
nazik <naziktajalsir1999@gmail.com>
2023-08-11 14:41:20 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
  * main - entry point
  *
- *Description: false or true validation
+ * Description: false or true validation
  *
- * Return : 0 (success)
+ * Return: 0 (success)
  */
 
 int main(void)
